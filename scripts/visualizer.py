e52d1a3afb6e0b6c35e8fbf3dcf1e325673c911a
nRknpy <ofuro.jabu.jabu@gmail.com>
2023-06-28 23:43:12 +0900

    add notebooks

--- scripts/visualizer.py
+++ scripts/visualizer.py
@@ -14,6 +14,19 @@ labeled_valence = {
 }
 
 
+def head_outputs(model, dataset, device):
+    features = []
+    labels = []
+    for img, label in tqdm(dataset):
+        if isinstance(img, tuple):
+            img = img[0]
+        with torch.no_grad():
+            feature = model(img.unsqueeze(0).to(device)).logits
+        features.append(feature.cpu())
+        labels.append(label)
+    return torch.stack(features).squeeze(), torch.tensor(labels)
+
+
 def CLS_tokens(model, dataset, device):
     tokens = []
     labels = []
@@ -28,6 +41,23 @@ def CLS_tokens(model, dataset, device):
     return torch.stack(tokens).squeeze(), torch.tensor(labels)
 
 
+def hidden_tokens(model, dataset, device):
+    tokens_list = [[] for _ in range(12)]
+    labels = []
+    for img, label in tqdm(dataset):
+        if isinstance(img, tuple):
+            img = img[0]
+        with torch.no_grad():
+            tokens = model(img.unsqueeze(0).to(device),
+                           output_hidden_states=True).hidden_states
+        for i in range(1, 13):
+            tokens_list[i-1].append(tokens[i][0, 0, :].cpu())
+        labels.append(label)
+    for i in range(len(tokens_list)):
+        tokens_list[i] = torch.stack(tokens_list[i])
+    return tokens_list, torch.tensor(labels)
+
+
 def plot_tokens_category(tokens, labels, n_neighbors, id2label, random_seed):
     umap = UMAP(n_neighbors=n_neighbors, random_state=random_seed)
     zs = umap.fit_transform(tokens.numpy())
@@ -37,9 +67,11 @@ def plot_tokens_category(tokens, labels, n_neighbors, id2label, random_seed):
     ax.set_xlabel('feature-1')
     ax.set_ylabel('feature-2')
     cmap = cm.get_cmap('gist_ncar')
+    ax.set_box_aspect(1)
 
     label2point = {}
-    for x, y in zip(zs, ys):
+    print('plotting tokens...')
+    for x, y in tqdm(zip(zs, ys)):
         mp = ax.scatter(x[0], x[1],
                         alpha=1,
                         label=id2label[y],
@@ -56,7 +88,7 @@ def plot_tokens_category(tokens, labels, n_neighbors, id2label, random_seed):
     return fig, legend
 
 
-def plot_tokens_continuous(tokens, targets, n_neighbors):
+def plot_tokens_continuous(tokens, targets, n_neighbors, random_seed):
     umap = UMAP(n_neighbors=n_neighbors)
     zs = np.array(umap.fit_transform(tokens.numpy()))
     x = zs[:, 0]
@@ -67,6 +99,7 @@ def plot_tokens_continuous(tokens, targets, n_neighbors):
     ax.set_aspect('equal', adjustable='box')
     ax.set_xlabel('feature-1')
     ax.set_ylabel('feature-2')
+    ax.set_box_aspect(1)
 
     mp = ax.scatter(x, y,
                     alpha=1,
@@ -76,4 +109,35 @@ def plot_tokens_continuous(tokens, targets, n_neighbors):
                     vmax=1,
                     s=3)
     fig.colorbar(mp, ax=ax)
+    return fig
+
+
+def plot_hidden_tokens_category(tokens_list, labels, n_neighbors, id2label, random_seed):
+    ys = labels.numpy()
+    vmax = len(set(ys))
+    fig = plt.figure(figsize=(100, 8))
+    fig.tight_layout()
+    cmap = cm.get_cmap('gist_ncar')
+    for i in tqdm(range(len(tokens_list))):
+        ax = fig.add_subplot(1, len(tokens_list), i+1)
+        ax.set_box_aspect(1)
+        ax.set_title(f'{i+1}', fontsize=100)
+        ax.axes.xaxis.set_ticks([])
+        ax.axes.yaxis.set_ticks([])
+        umap = UMAP(n_neighbors=n_neighbors, random_state=random_seed)
+        zs = umap.fit_transform(tokens_list[i].numpy())
+        label2point = {}
+        for x, y in zip(zs, ys):
+            mp = ax.scatter(x[0], x[1],
+                            alpha=1,
+                            label=id2label[y],
+                            c=y,
+                            cmap=cmap,
+                            vmin=0,
+                            vmax=vmax,
+                            s=20,)
+            label2point[id2label[y]] = mp
+        labels_, handles = zip(*sorted(label2point.items()))
+    # legend = fig.legend(
+    #     handles, labels_, loc='upper center', ncol=8, fontsize=100)
     return fig
