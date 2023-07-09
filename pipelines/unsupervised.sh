04268fa2f5d500b9a077d1e69f85b67705bc1927
nRknpy <ofuro.jabu.jabu@gmail.com>
2023-07-09 10:29:14 +0900

    fix supcon trainer

---
+++ pipelines/unsupervised.sh
@@ -0,0 +1,3 @@
+python scripts/main_contrastive.py exp=CL_unsupervised_z64_bs512
+python scripts/main_finetuning.py exp=FT_unsupervised_z64_bs512
+python scripts/main_finetuning.py exp=FT_head_unsupervised_z64_bs512
